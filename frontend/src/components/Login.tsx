5fdb2f02332075800e202c1942835121af63a553
Lara Rodrigues <lararfonseca16@gmail.com>
2026-08-29 19:29:22 -0300

    feat: melhora visual da tela de login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,6 +1,9 @@
 import { useState, type FormEvent } from "react";
+import {FiUser, FiLock} from "react-icons/fi";
+import { MdOutlineChat } from "react-icons/md";
 import type { LoginResponse } from "@agentic/shared";
 import { login } from "../api.js";
+
 
 export function Login({ onEntrar }: { onEntrar: (s: LoginResponse) => void }) {
   const [username, setUsername] = useState("alice");
@@ -20,25 +23,36 @@ export function Login({ onEntrar }: { onEntrar: (s: LoginResponse) => void }) {
  return (
   <main className="login-page">
     <form className="login-form" onSubmit={enviar}>
-      <h1>Agentic Payments</h1>
-      <p>Entre para acessar o chat de pagamentos</p>
+      <div className="login-icon"><MdOutlineChat></MdOutlineChat></div>
+      <h1 className="login-title">Agentic Payments</h1>
+      <p className="login-subtitle">Entre para acessar o chat de pagamentos</p>
 
       <label>
-        Usuário
+        Usuário:
+
+        <div className="input-wrapper">
+          <FiUser className="input-icon"></FiUser>
+
         <input value={username}
         onChange={(e) => setUsername(e.target.value)}
-        placeholder="Digite sua senha"
+        placeholder="Digite seu nome de usuário"
         />
+        </div>
       </label>
 
       <label>
-        Senha
+        Senha:
+
+        <div className="input-wrapper">
+          <FiLock className="input-icon"></FiLock>
+
         <input 
         type="password"
         value={senha}
         onChange={(e) => setSenha(e.target.value)} 
         placeholder="Digite sua senha"
         />
+        </div>
       </label>
 
       <button type="submit">Entrar</button>
